fd4502ba4ad5aa8ad343a6b40939b2864d63356e
kurpleClimax <thiagorobertosalomao5@gmail.com>
2024-10-27 00:41:03 +0900

    init

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Exchanges = () => {
+  return (
+    <div className='flex justify-center items-center text-4xl mt-72 dark:text-gray-200'>Sorry! Nothing to show.</div>
+  )
+}
+
+export default Exchanges
